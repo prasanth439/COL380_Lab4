d441771d92eefb2c83b2f79203edef916f92f115
prasanth439 <prasanth439@gmail.com>
2019-04-22 20:11:27 +0530

    rough addition of functions

--- mpi_hello.c
+++ mpi_hello.c
@@ -62,11 +62,11 @@ np_text_analysis(char* text,
                 int* witness_function,
                 int text_length,
                 int pattern_length,
-                int witness_size
-                )
+                int witness_size)
 {
-    const int block_size = ceil(m/2);
-    int num_blocks = n/block_size,block_offset_limit;
+    const int block_size = ceil(pattern_length/2.0);
+    int num_blocks = text_length/block_size;
+    int block_offset_limit;
     int* potential_pos = (int*)malloc(sizeof(int)*(num_blocks));
     int* match_pos = (int*)malloc(sizeof(int)*(num_blocks+1));
     match_pos[0]=0;
@@ -76,20 +76,20 @@ np_text_analysis(char* text,
         i = block_size*k;
         if(k==num_blocks-1)
         {
-            block_offset_limit = n;
+            block_offset_limit = text_length;
         }
         else{
             block_offset_limit = (k+1)*block_size;
         }
         for(int j=i+1;j<block_offset_limit;j++)
         {
-            i = duelCompute(text,pattern,witness_function,i,j,n,m);
+            i = duelCompute(text,pattern,witness_function,i,j,text_length);
         }
         potential_pos[k] = i;
     }
     for(int i=0;i<num_blocks;i++)
     {
-        if(check_pattern(text,pattern,potential_pos[i],m))
+        if(check_pattern(text,pattern,potential_pos[i],pattern_length,text_length))
         {
             match_pos[0]++;
             match_pos[match_pos[0]] = potential_pos[i];
@@ -107,9 +107,47 @@ p_text_analysis(char* text_str,
                 int period)
 {
     int p = period;
-    int* witF = (int*)malloc(sizeof(int)*p);
-    witness_compute(patt_str,patt_length,witF,p);
-    int * pos = np_text_analysis(text_str,patt_str,witF,text_length,patt_length,p);
+    int* witness_function = (int*)malloc(sizeof(int)*p);
+    witness_compute(patt_str,patt_length,witness_function,p);
+    int * pos = np_text_analysis(text_str,patt_str,witness_function,text_length,patt_length,p);
+    int u =0; //0 to p-1 
+    int k = (int)ceil(patt_length/(double)p);
+    int v =0; // kp to m-1
+    int* M = (int*)malloc(sizeof(int)*text_length);
+    int* S = (int*)malloc(sizeof(int)*p);
+    int** C;
+    int* MATCH = (int*)malloc(sizeof(int)*(text_length-patt_length));
+    for(int i=0;i<text_length;i++)
+    {
+        M[i] = 0;
+    }
+    for(int i=1;i<=pos[0];i++)
+    {
+        // check the u2v at i
+        M[i] = 1;
+    }
+    for(int i =0;i<p;i++)
+    {
+        S[i] = 0;
+        for(int j=0;j<S[i];j++)
+        {
+            C[i][j] = 0;
+            if(/*k-1 consecutive 1s starting at i*/1)
+            {
+                C[i][j] = 1;
+            }
+        }
+    }
+    for(int j=0;j<=text_length-patt_length;j++)
+    {
+        int i = 0;
+        int l = 0;
+        if(/*something something*/1)
+        {
+            MATCH[j] = C[i][l];
+        }
+    }
+    return 0;
     // to write it //
 };
 
@@ -119,7 +157,6 @@ int main(int argc, char** argv) {
     // MPI_Init(argc, argv);
 
     // duelCompute(text,pattern,pi,1,2,t_L,p_L);
-
     // MPI_Finalize();
     return 0;
 };
